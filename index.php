d8b1af927c84d2318a74e1c09270381a80c200f6
luoluodeshijie <563959942@qq.com>
2017-09-18 01:29:19 +0800

    初始化V1.0.0版本

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php 
+  //后端路由（根据URL的不同相应不同的页面）
+  header('content-type:text/html;charset=utf8');
+  //include('./header.html');
+  //echo '<div>主页内容</div>';
+  //include('./footer.html');
+  //include 在当前PHP页面内部嵌入一个子页面
+  //必须能够通过URL区分出用户想访问的那个页面
+  
+  //默认目录名称
+  $dir = 'main';
+  //默认文件名称
+  $filename = 'index';
+
+  //处理URL的路径
+  if(array_key_exists('PATH_INFO', $_SERVER)){
+  	//PATH_INFO属性存在
+  	//获取请求路径
+  	$path = $_SERVER['PATH_INFO'];//  /main/index
+  	//去掉第一个斜杠
+  	$str = substr($path, 1);//  main/index
+  	//字符串分割，和js中split方法很像
+  	$ret = explode('/',$str);
+  	if (count($ret) ==2) {
+  		//路径有两层
+  		$dir = $ret[0];//覆盖目录
+  		$filename = $ret[1];//覆盖文件名称
+  	}else{
+  		//其他情况全部挑战到登陆页面
+  		$filename = 'login';
+  	}
+  }
+
+  //include('./views/main/index.html');
+  //var_dump($_SERVER); //本以上是个数组
+  //
+  //echo $path;
+  //嵌入子页面
+  
+  include('./views/'.$dir.'/'.$filename.'.html');
+
+
+
+
+?>
