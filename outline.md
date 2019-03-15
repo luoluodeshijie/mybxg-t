5950e1aaad48154f21086a49548a28044ac44fc0
limingxian <limingxianweb@163.com>
2019-03-15 23:44:00 +0800

    v1.0.0.2

---
+++ outline.md
@@ -0,0 +1,23 @@
+# 大纲
+
+## [模板引擎](https://github.com/aui/art-template)
+### artTemplate基本语法
+
+### artTemplate的API
+
+## 讲师列表加载
+
+## Bootstrap模态框基本使用
+
+## 查看讲师功能
+
+## 启用和注销讲师
+
+## 左侧菜单选中设置
+
+
+
+
+
+
+
--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,35 @@
+NProgress.start();
+NProgress.done();
 
-	NProgress.start();
+$('.navs ul').prev('a').on('click', function() {
+    $(this).next().slideToggle();
+});
 
-	NProgress.done();
+// 实现退出功能
+$('#logoutBtn').click(function() {
+    $.ajax({
+        type: 'post',
+        url: '/api/logout',
+        dataType: 'json',
+        success: function(data) {
+            if (data.code == 200) {
+                // 重新跳转到登录页
+                location.href = '/main/login'
+            }
+        }
+    })
+})
 
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+// 检测用户是否登录
+var flag = $.cookie('PHPSESSID')
+if (!flag) {
+    // 如果 cookie 不存在，跳转到登录页
+    location.href = '/main/login'
+}
+
+// 设置用户头像信息
+var loginInfo = $.cookie('loginInfo')
+loginInfo = loginInfo && JSON.parse(loginInfo)
+    //设置用户的头像信息
+$('.aside .profile img').attr('src', loginInfo.tc_avatar)
+$('.aside .profile h4').html(loginInfo.tc_name)
