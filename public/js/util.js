d2b3b667a9df8e00e445469e40ed658117b7b51a
limingxian <limingxianweb@163.com>
2019-03-17 13:06:15 +0800

    实现添加和编辑讲师页面共用

---
+++ public/js/util.js
@@ -0,0 +1,20 @@
+define(['jquery'], function($) {
+    return {
+        qs: function(key) {
+            // 获取地址的参数，并且截取掉 ?
+            var param = location.search.substr(1) // tc_id=1&flag=5
+            var result = null
+            if (param) {
+                var ps = param.split('&') // ['tc_id=1', 'flag=5']
+                $.each(ps, function(index, item) {
+                    var kv = item.split('=') // ['tc_id', '1', 'flag', '5']
+                    if (kv[0] == key) {
+                        result = kv[1]
+                        return false // 终止 each 循环
+                    }
+                })
+            }
+            return result
+        }
+    }
+})
