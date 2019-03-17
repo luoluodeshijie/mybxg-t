18a6eb79072c4b0b07f2645db48f92a14716ccac
limingxian <limingxianweb@163.com>
2019-03-17 21:34:37 +0800

    添加表单验证和提交插件

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,4 +1,4 @@
-define(['jquery', 'template', 'util', 'datepicker', 'language'], function($, template, util) {
+define(['jquery', 'template', 'util', 'datepicker', 'language', 'validate', 'form'], function($, template, util) {
     var tcId = util.qs('tc_id')
     if (tcId) {
         // 编辑操作
@@ -24,21 +24,51 @@ define(['jquery', 'template', 'util', 'datepicker', 'language'], function($, tem
         submitForm('/api/teacher/add')
     }
 
-    // 实现表单提交
     function submitForm(url) {
-        $('#teacherBtn').click(function() {
-            $.ajax({
-                type: 'post',
-                url: url,
-                data: $('#teacherForm').serialize(),
-                dataType: 'json',
-                success: function(data) {
-                    if (data.code == 200) {
-                        location.href = '/teacher/list'
+        $('#teacherForm').validate({
+            sendForm: false,
+            valid: function() {
+                $(this).ajaxSubmit({
+                    type: 'post',
+                    url: url,
+                    dataType: 'json',
+                    success: function(data) {
+                        if (data.code == 200) {
+                            location.href = '/teacher/list'
+                        }
                     }
+                })
+            },
+            description: {
+                tcName: {
+                    required: '用户名不能为空'
+                },
+                tcPass: {
+                    required: '密码不能为空',
+                    pattern: '密码必须为6位数字'
+                },
+                tcJoinDate: {
+                    required: '日期不能为空'
                 }
-
-            })
+            }
         })
     }
+    // 实现表单提交
+    // function submitForm(url) {
+    //     $('#teacherBtn').click(function() {
+    //         $.ajax({
+    //             type: 'post',
+    //             url: url,
+    //             data: $('#teacherForm').serialize(),
+    //             dataType: 'json',
+    //             success: function(data) {
+    //                 if (data.code == 200) {
+    //                     location.href = '/teacher/list'
+    //                 }
+    //             }
+
+    //         })
+    //     })
+    // }
+
 })
