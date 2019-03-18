9945859b897300d1165d69942d1bccd3300eaa56
limingxian <limingxianweb@163.com>
2019-03-18 23:15:44 +0800

    完成个人中心表单提交功能

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery', 'template', 'uploadify', 'region'], function($, template) {
+define(['jquery', 'template', 'ckeditor', 'uploadify', 'region', 'datepicker', 'language', 'validate', 'form'], function($, template, CKEDITOR) {
     // 调用接口获取个人信息
     $.ajax({
         type: 'get',
@@ -28,6 +28,73 @@ define(['jquery', 'template', 'uploadify', 'region'], function($, template) {
             // 处理省市县三级联动
             $('#pcd').region({
                 url: '/public/assets/jquery-region/region.json'
+            })
+
+            // 处理富文本
+            CKEDITOR.replace('editor', {
+                toolbarGroups: [{
+                        name: 'clipboard',
+                        groups: ['clipboard', 'undo']
+                    }, {
+                        name: 'editing',
+                        groups: ['find', 'selection', 'spellchecker', 'editing']
+                    }, {
+                        name: 'links',
+                        groups: ['links']
+                    }, {
+                        name: 'insert',
+                        groups: ['insert']
+                    }, {
+                        name: 'forms',
+                        groups: ['forms']
+                    }, {
+                        name: 'tools',
+                        groups: ['tools']
+                    }, {
+                        name: 'document',
+                        groups: ['mode', 'document', 'doctools']
+                    }, {
+                        name: 'others',
+                        groups: ['others']
+                    },
+                    '/', {
+                        name: 'basicstyles',
+                        groups: ['basicstyles', 'cleanup']
+                    }, {
+                        name: 'paragraph',
+                        groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph']
+                    }
+                ]
+            })
+
+
+            // 处理表单提交
+            $('#settingsForm').validate({
+                sendForm: false,
+                valid: function() {
+                    // 获取家乡信息
+                    var p = $('#p').find('option:selected').text()
+                    var c = $('#c').find('option:selected').text()
+                    var d = $('#d').find('option:selected').text()
+                    var hometown = p + '|' + c + '|' + d
+                        // 同步富文本内容
+                    for (var instance in CKEDITOR.instances) {
+                        CKEDITOR.instances[instance].updateElement()
+                    }
+                    // 提交表单
+                    $(this).ajaxSubmit({
+                        type: 'post',
+                        url: '/api/teacher/modify',
+                        data: { tc_hometown: hometown },
+                        dataType: 'json',
+                        success: function(data) {
+                            if (data.code == 200) {
+                                // 修改成功之后重新刷新页面
+                                location.reload()
+                            }
+                        }
+                    })
+                }
             })
 
         }
