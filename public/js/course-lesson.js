e16b67ef46ed6152abd90e9618d52f10cc047337
limingxian <limingxianweb@163.com>
2019-04-21 00:51:21 +0800

    完成课时添加和编辑的弹窗共享

--- public/js/course-lesson.js
+++ public/js/course-lesson.js
@@ -1,4 +1,4 @@
-define(['jquery', 'template', 'util'], function($, template, util) {
+define(['jquery', 'template', 'util', 'bootstrap'], function($, template, util) {
     // 设置导航菜单选中
     util.setMenu('/course/add')
         // 获取课程ID
@@ -10,10 +10,41 @@ define(['jquery', 'template', 'util'], function($, template, util) {
         data: { cs_id: csId },
         dataType: 'json',
         success: function(data) {
-            console.log(data);
+            // console.log(data);
             // 解析数据，渲染页面
             var html = template('lessonTpl', data.result)
-            $('#lessonInfo').html(html)
+            $('#lessonInfo').html(html);
+
+            // 绑定添加课时事件
+            $('#addLesson').click(function() {
+                // 解析数据，渲染页面
+                var html = template('modalTpl', { operate: '添加课时' })
+                $('#modalInfo').html(html)
+                    // 显示模态框
+                $('#chapterModal').modal()
+            });
+
+
+            // 绑定编辑课时事件
+            $('.editBtn').click(function() {
+                // 获取课时ID
+                var ctId = $(this).attr('data-ctId')
+                    // 根据课时ID查询课时数据信息
+                $.ajax({
+                    type: 'get',
+                    url: '/api/course/chapter/edit',
+                    data: { ct_id: ctId },
+                    dataType: 'json',
+                    success: function(data) {
+                        // 解析数据，渲染页面
+                        data.result.operate = '编辑课时'
+                        var html = template('modalTpl', data.result)
+                        $('#modalInfo').html(html)
+                            // 显示模态框
+                        $('#chapterModal').modal()
+                    }
+                })
+            })
         }
     })
 
